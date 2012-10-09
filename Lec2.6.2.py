3c62327e25b3bc230fc760bd4f4e5c9ce88bfc32
Gordon Greeff <gordon@shrubbery.org.za>
2012-10-09 21:22:59 +0200

    Just adding some stuff

---
+++ Lec2.6.2.py
@@ -0,0 +1,13 @@
+## Script to check if a number is even
+
+integer = int (raw_input ('Enter an integer: '))
+
+if integer%2 == 0:
+	print ('')
+	print ('Your number is even')
+
+else:
+	print ('')
+	print ('Your number is odd')
+
+print ('Done with conditional')
