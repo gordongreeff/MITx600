3df610c7251280333990000365cf888a22b2bbba
Gordon Greeff <gordon@shrubbery.org.za>
2012-10-15 13:49:17 +0200

    Next scratches

--- scratch.py
+++ scratch.py
@@ -1,7 +1,9 @@
-temp = '32'
-if temp > 85:
-   print('Hot')
-elif temp > 62:
-   print('Comfortable')
-else:
-   print('Cold')
+iteration = 0
+while iteration < 5:
+    count = 0
+    for letter in "hello, world":
+        count += 1
+        if iteration % 2 == 0:
+            break
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1
